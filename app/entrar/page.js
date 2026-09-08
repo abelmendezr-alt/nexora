5b75e20734a99fab561c69c95bb3f3fdad29e4b2
abelmendezr-alt <abel.mendez.r@gmail.com>
2026-09-07 20:16:38 -0600

    page.js

--- app/entrar/page.js
+++ app/entrar/page.js
@@ -1,13 +1,9 @@
-import Link from "next/link";
-
-export default function Home() {
+export default function Entrar() {
   return (
     <main>
       <h1>NEXORA</h1>
-      <p>Todo está conectado.</p>
-
-      <Link href="/entrar">Entrar</Link>
-      <Link href="/explorar">Explorar el nexo</Link>
+      <p>Entra al nexo.</p>
+      <button>Continuar</button>
     </main>
   );
 }
