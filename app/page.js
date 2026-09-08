9e5c72667665a488e03fe1262cec313ecaafdfa3
abelmendezr-alt <abel.mendez.r@gmail.com>
2026-09-08 02:30:17 -0600

    page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,50 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <main>
-      <h1>NEXORA</h1>
-      <p>Todo está conectado.</p>
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#050505",
+        color: "white",
+        textAlign: "center",
+        padding: "24px",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "64px",
+          letterSpacing: "12px",
+          marginBottom: "16px",
+        }}
+      >
+        NEXORA
+      </h1>
 
-      <Link href="/entrar">Entrar</Link>
-      <br />
-      <Link href="/nexo">Explorar el nexo</Link>
+      <p
+        style={{
+          fontSize: "20px",
+          opacity: 0.7,
+          marginBottom: "40px",
+        }}
+      >
+        Todo está conectado.
+      </p>
+
+      <a
+        href="/entrar"
+        style={{
+          padding: "14px 32px",
+          border: "1px solid white",
+          borderRadius: "30px",
+          color: "white",
+          textDecoration: "none",
+        }}
+      >
+        Entrar
+      </a>
     </main>
   );
 }
