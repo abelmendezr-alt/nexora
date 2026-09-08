a5a4ac2f76d5f3ab07179f1c29820c397f4b8232
abelmendezr-alt <abel.mendez.r@gmail.com>
2026-09-07 22:51:55 -0600

    data.js

--- data.js
+++ data.js
@@ -1,3 +1,16 @@
 export const nexoraData = {
-  publicaciones: [],
+  publicaciones: [
+    {
+      id: 1,
+      texto: "La vida sabe a pepino.",
+    },
+    {
+      id: 2,
+      texto: "Todo está conectado.",
+    },
+    {
+      id: 3,
+      texto: "El nexo se expande.",
+    },
+  ],
 };
