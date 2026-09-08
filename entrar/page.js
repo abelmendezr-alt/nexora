66eeac13d92a595a1ef88552d9b9f3dbc79d7745
abelmendezr-alt <abel.mendez.r@gmail.com>
2026-09-07 19:10:19 -0600

    page.js

---
+++ entrar/page.js
@@ -0,0 +1,15 @@
+export default function Entrar() {
+  return (
+    <main>
+      <h1>NEXORA</h1>
+      <p>Entra al nexo.</p>
+
+      <input
+        type="text"
+        placeholder="Tu nombre"
+      />
+
+      <button>Continuar</button>
+    </main>
+  );
+}
