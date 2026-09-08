35f8de8152b1607f8e464a89878cfe0ba3cd4932
abelmendezr-alt <abel.mendez.r@gmail.com>
2026-09-07 21:20:08 -0600

    page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>NEXORA</h1>
       <p>Todo está conectado.</p>
 
-      <a href="/entrar">ENTRAR</a>
+      <Link href="/entrar">Entrar</Link>
+      <Link href="/nexo">Explorar el nexo</Link>
     </main>
   );
 }
